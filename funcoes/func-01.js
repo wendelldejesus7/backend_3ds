4012f4218e61d69513712fef9c3f37a25326717d
wendelldejesus7 <goncalves.wendell@escola.pr.gov.br>
2026-03-31 10:50:13 +0000

    arrumei o commit

---
+++ funcoes/func-01.js
@@ -0,0 +1,16 @@
+// Funções são blocos de código reaproveitáveis
+// podem ou Não receber parâmetros
+// podem ou não retornar valores
+
+// declaraçao
+function dizOla(){
+    console.log("Olá")
+}
+function olaPessoa(nome){
+    console.log("Ola, " + nome)
+}
+
+// invocação
+dizOla()
+olaPessoa("Luana")
+olaPessoa("Mano")
