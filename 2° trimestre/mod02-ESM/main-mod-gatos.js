20c8c62f5329e8c3a8a2536e5f6799d4b27e00e0
wendelldejesus7 <goncalves.wendell@escola.pr.gov.br>
2026-05-13 11:44:02 +0000

    Finalizaçao ESM módulo Gatos

---
+++ 2° trimestre/mod02-ESM/main-mod-gatos.js
@@ -0,0 +1,7 @@
+import {banhoGato, tosaGato, alimentarGato} from './mod-gatos.js'
+
+let racaViraLata = 'Vira-Lata'
+
+console.log(banhoGato('Siames' , 'Completo'))
+console.log(tosaGato('Siames' , 'Completo'))
+    console.log(alimentarGato(  'Siames' , 'Completa'))
---
+++ 2° trimestre/mod02-ESM/mod-gatos.js
@@ -0,0 +1,13 @@
+let racaViraLata = 'Vira-Lata'
+
+export function banhoGato(racaGato, tipoBanho) {
+return `Gato ${racaGato} fez um banho tipo: ${tipoBanho}`
+}
+
+export function tosaGato(racaGato, tipotosa) {
+return `Gato ${racaGato} fez uma tosa tipo: ${tipotosa}`
+}
+
+export function alimentarGato(racaGato, tipoRacao) {
+return `Gato ${racaGato} fez uma alimentaçao tipo: ${tipoRacao}`
+}
