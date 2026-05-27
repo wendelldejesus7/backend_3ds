6f4c2abc634edfadda43ac1efd938b4437954a1a
wendelldejesus7 <goncalves.wendell@escola.pr.gov.br>
2026-05-27 11:49:42 +0000

    finalizaçao do miodo FS com Path (join e resolve)

--- 2° trimestre/mod03-FS/fs-com-path.js
+++ 2° trimestre/mod03-FS/fs-com-path.js
@@ -1,9 +1,15 @@
 // importar PATH E FS
+// Path possui (join para juntar caminhos e Resolve)
+// para deduzir subdiretórios e 5.0.)
+const fs = require('fs')
+const path = require('path')
 
-
+//console.log(path)
 
 // Salvar o Caminho em um variavel
+//const caminho = path.join(__dirname, 'README.md')//Markdown, text,
+//console.log(caminho)
 
-
-
-// Abrir, ler, deletar, editar
+const caminho = path.resolve (__dirname, 'README.md' ) // Abrir, ler, deletar, editar
+const arquivo = fs.readFileSync(caminho, 'utf-8')
+console.log(arquivo)
